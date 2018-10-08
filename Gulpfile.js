02fca09bb9851bc3e4dbcfaa5e8f94efc1ddc98d
Stef Coenen <stef.coenen@gmail.com>
2018-10-08 19:45:14 +0200

    Updated gulpfile to last revision;

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,15 +4,16 @@ const sass = require('gulp-sass'); // Sass compiler
 const autoprefixer = require('gulp-autoprefixer'); // Fixed vendor prefixes by required prefixes for configured compatibility
 const sourcemaps = require('gulp-sourcemaps'); // Adds sourcemaps to your resulting css file
 const babel = require('gulp-babel'); // ES6 Transpilation
-const uglify = require('gulp-uglify'); // JS Minification
+const uglify = require('gulp-uglify-es').default; // JS Minification
 const concat = require('gulp-concat'); // JS Concatenation
 const cleanCSS = require('gulp-clean-css'); // CSS Minification
 const nunjucksRender = require('gulp-nunjucks-render'); // Nunjucks renderer
 const wbBuild = require('workbox-build'); // Generates ServiceWorker on static assets
 const express = require('express'); // Webserver
+const browserSync = require('browser-sync').create();
 const path = require('path'); // NPM path helper
 
-const DEV_DIR = '';
+const DEV_DIR = 'app';
 const DESIGN_DIR = path.join(DEV_DIR, 'design');
 const DIST_DIR = 'docs';
 
@@ -34,26 +35,43 @@ function runExpress(port, rootDir) {
     });
 }
 
+function runBrowserSync(rootDir) {
+    browserSync.init({
+        server: {
+            baseDir: rootDir
+        },
+        open: false
+    });
+}
+
 gulp.task('copy-dependencies', function() {
-    gulp
-        .src(['./node_modules/jquery/dist/jquery.js'])
-        .pipe(gulp.dest(path.join(DESIGN_DIR, 'js/lib/jquery')));
+    gulp.src(['./node_modules/jquery/dist/jquery.js']).pipe(
+        gulp.dest(path.join(DESIGN_DIR, 'js/lib/jquery'))
+    );
 
-    gulp
-        .src([
-            './node_modules/bootstrap/js/dist/util.js',
-            './node_modules/bootstrap/js/dist/collapse.js'
-        ])
-        .pipe(gulp.dest(path.join(DESIGN_DIR, 'js/lib/bootstrap')));
+    gulp.src([
+        './node_modules/bootstrap/js/dist/util.js',
+        './node_modules/bootstrap/js/dist/collapse.js',
+        './node_modules/bootstrap/js/dist/alert.js',
+        './node_modules/bootstrap/js/dist/dropdown.js',
+        './node_modules/bootstrap/js/dist/modal.js'
+    ]).pipe(gulp.dest(path.join(DESIGN_DIR, 'js/lib/bootstrap')));
 
-    gulp
-        .src(['./node_modules/bootstrap/scss/**/_*.scss'])
-        .pipe(gulp.dest(path.join(DESIGN_DIR, 'scss/vendor/bootstrap')));
+    gulp.src(['./node_modules/bootstrap/scss/**/_*.scss']).pipe(
+        gulp.dest(path.join(DESIGN_DIR, 'scss/vendor/bootstrap'))
+    );
+
+    gulp.src(['./node_modules/popper.js/dist/umd/popper.js']).pipe(
+        gulp.dest(path.join(DESIGN_DIR, 'js/lib/popper'))
+    );
+
+    gulp.src(['./node_modules/@glidejs/glide/dist/glide.js']).pipe(
+        gulp.dest(path.join(DESIGN_DIR, 'js/lib/glide'))
+    );
 });
 
 gulp.task('stylesheets', function() {
-    gulp
-        .src(path.join(DESIGN_DIR, 'scss/**/*.scss'))
+    gulp.src(path.join(DESIGN_DIR, 'scss/**/*.scss'))
         .pipe(sourcemaps.init())
         .pipe(
             sass({
@@ -62,69 +80,73 @@ gulp.task('stylesheets', function() {
         )
         .pipe(
             autoprefixer({
-                browsers: ['last 2 versions', 'ie >= 8', 'ios >= 8'],
+                browsers: ['last 2 versions', 'ie >= 10', 'ios >= 9'],
                 cascade: false
             })
         )
         .pipe(
             cleanCSS({
-                compatibility: 'ie8'
+                compatibility: 'ie10'
             })
         )
-        // .pipe(
-        //     sourcemaps.write({
-        //         sourceRoot: './design/scss/'
-        //     })
-        // )
-        .pipe(gulp.dest(path.join(DIST_DIR, 'design/css/')));
+        .pipe(
+            sourcemaps.write('maps', {
+                sourceRoot: './design/scss/'
+            })
+        )
+        .pipe(gulp.dest(path.join(DIST_DIR, 'design/css/')))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('javascript', function() {
-    gulp
-        .src([
-            path.join(DESIGN_DIR, 'js/lib/jquery/*.js'),
-            path.join(DESIGN_DIR, 'js/lib/bootstrap/**/*.js')
-        ])
+gulp.task('javascript-libs', function() {
+    gulp.src([
+        //path.join(DESIGN_DIR, 'js/lib/jquery/*.js'),
+        path.join(DESIGN_DIR, 'js/lib/glide/glide.js')
+        // path.join(DESIGN_DIR, 'js/lib/popper/popper.js'), //kill gulp minify
+        // path.join(DESIGN_DIR, 'js/lib/magnific-popup/jquery.magnific-popup.js'),
+        // path.join(DESIGN_DIR, 'js/lib/bootstrap/**/*.js')
+    ])
         .pipe(concat('lib.js'))
         .pipe(babel())
         .pipe(uglify())
         .pipe(gulp.dest(path.join(DIST_DIR, 'design/js/')));
+});
 
-    gulp
-        .src([
-            path.join(DESIGN_DIR, 'js/app/components/*.js'),
-            path.join(DESIGN_DIR, 'js/app/functions/*.js'),
-            path.join(DESIGN_DIR, 'js/app/modules/*.js'),
-            path.join(DESIGN_DIR, 'js/app/lube.strapon.js')
-        ])
+gulp.task('javascript', function() {
+    gulp.src([
+        path.join(DESIGN_DIR, 'js/app/components/*.js'),
+        path.join(DESIGN_DIR, 'js/app/functions/*.js'),
+        path.join(DESIGN_DIR, 'js/app/modules/*.js'),
+        path.join(DESIGN_DIR, 'js/app/lube.strapon.js')
+    ])
+        .pipe(sourcemaps.init())
         .pipe(concat('applib.js'))
         .pipe(babel())
         .pipe(uglify())
-        .pipe(gulp.dest(path.join(DIST_DIR, 'design/js/')));
+        .pipe(sourcemaps.write(path.join(DIST_DIR, 'design/js/maps/')))
+        .pipe(gulp.dest(path.join(DIST_DIR, 'design/js/')))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('copy-img', function() {
-    gulp
-        .src(path.join(DESIGN_DIR, 'img/**/*'))
-        .pipe(gulp.dest(path.join(DIST_DIR, 'design/img')));
+    gulp.src(path.join(DESIGN_DIR, 'img/**/*')).pipe(gulp.dest(path.join(DIST_DIR, 'design/img')));
 });
 
 gulp.task('copy-icons', function() {
-    gulp
-        .src(path.join(DESIGN_DIR, 'favicons/**/*'))
-        .pipe(gulp.dest(path.join(DIST_DIR, 'design/favicons')));
+    gulp.src(path.join(DESIGN_DIR, 'favicons/**/*')).pipe(
+        gulp.dest(path.join(DIST_DIR, 'design/favicons'))
+    );
 });
 
 gulp.task('copy-fonts', function() {
-    gulp
-        .src(path.join(DESIGN_DIR, 'fonts/**/*'))
-        .pipe(gulp.dest(path.join(DIST_DIR, 'design/fonts')));
+    gulp.src(path.join(DESIGN_DIR, 'fonts/**/*')).pipe(
+        gulp.dest(path.join(DIST_DIR, 'design/fonts'))
+    );
 });
 
 gulp.task('compile-nunjucks', function() {
-    // Gets .html and .nunjucks files in pages
-    gulp
-        .src(path.join(DEV_DIR, 'app/pages/**/*.+(html|nunjucks)'))
+    // Gets .html and .njk files in pages
+    gulp.src(path.join(DEV_DIR, 'pages/**/*.+(html|njk)'))
         // Renders template with nunjucks
         .pipe(
             nunjucksRender({
@@ -132,7 +154,8 @@ gulp.task('compile-nunjucks', function() {
             })
         )
         // output files in app folder
-        .pipe(gulp.dest(DIST_DIR));
+        .pipe(gulp.dest(DIST_DIR))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('bundle-sw', () => {
@@ -152,12 +175,9 @@ gulp.task('bundle-sw', () => {
         });
 });
 
-// gulp.task('serve-dev', ['generate-service-worker-dev'], function () {
-//     runExpress(3001, DEV_DIR);
-// });
-
 gulp.task('serve-dist', [], function() {
-    runExpress(3000, DIST_DIR);
+    //runExpress(3000, DIST_DIR);
+    runBrowserSync(DIST_DIR);
 });
 
 gulp.on('err', function(err) {
@@ -165,19 +185,11 @@ gulp.on('err', function(err) {
 });
 
 //Watch task
-gulp.task('default', ['serve-dist'], function() {
-    gulp.watch(path.join(DESIGN_DIR, 'scss/**/*.scss'), [
-        'stylesheets',
-        'bundle-sw'
-    ]);
-    gulp.watch(path.join(DESIGN_DIR, 'js/**/*.js'), [
-        'javascript',
-        'bundle-sw'
-    ]);
-    gulp.watch(path.join(DEV_DIR, '**/*.+(html|nunjucks)'), [
-        'compile-nunjucks',
-        'bundle-sw'
-    ]);
+gulp.task('default', ['serve-dist'], () => {
+    gulp.watch(path.join(DESIGN_DIR, 'scss/**/*.scss'), ['stylesheets', 'bundle-sw']);
+    gulp.watch(path.join(DESIGN_DIR, 'js/app/**/*.js'), ['javascript', 'bundle-sw']);
+    gulp.watch(path.join(DESIGN_DIR, 'js/lib/**/*.js'), ['javascript-libs', 'bundle-sw']);
+    gulp.watch(path.join(DEV_DIR, '**/*.+(html|njk)'), ['compile-nunjucks', 'bundle-sw']);
 
     gulp.watch(path.join(DESIGN_DIR, 'img/**/*'), ['copy-img']);
     gulp.watch(path.join(DESIGN_DIR, 'favicons/**/*'), ['copy-icons']);
